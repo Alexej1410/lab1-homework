7245618a07a3e5fd0c0e743635e6b8f35c7d2199
Alexej1410 <kravchenko.alek5ej@yandex.com>
2025-09-22 13:09:15 +0300

    Add files via upload

---
+++ lab1-h.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts("               _  _");
+	puts("              /|  |\*");
+	puts("             / |  | \*");
+	puts("            /  |  |  \*");
+	puts("           /   |  |   \*");
+	puts("          /____|  |    \*");
+	puts("               |  |     \*");
+	puts("  |-----|      |  |______\*");
+	puts("  |*  Î  L_____|__|__________/-/");
+	puts("   L*    _   _   _   _   _    /");
+	puts("    L*   Î   Î   Î   Î   Î   /");
+	puts("   / L______________________/");
+    
+}
